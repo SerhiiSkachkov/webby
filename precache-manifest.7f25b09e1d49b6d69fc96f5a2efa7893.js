feb34a97afdee754e024068e9862c6d9bd9c29ed
SerhiiSkachkov <sergeyskachkov01@gmail.com>
2020-08-26 16:41:47 +0300

    Updates

---
+++ precache-manifest.7f25b09e1d49b6d69fc96f5a2efa7893.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca9780f77e5398ff99e5852cccb9904",
+    "url": "/SerhiiSkachkov/webby/index.html"
+  },
+  {
+    "revision": "12159fdb60c7cc7e41d0",
+    "url": "/SerhiiSkachkov/webby/static/css/main.04346068.chunk.css"
+  },
+  {
+    "revision": "008d5a011654a043a601",
+    "url": "/SerhiiSkachkov/webby/static/js/2.1411621b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SerhiiSkachkov/webby/static/js/2.1411621b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12159fdb60c7cc7e41d0",
+    "url": "/SerhiiSkachkov/webby/static/js/main.059df308.chunk.js"
+  },
+  {
+    "revision": "445f90f679ac5f07f4f6",
+    "url": "/SerhiiSkachkov/webby/static/js/runtime-main.9ce2a804.js"
+  },
+  {
+    "revision": "72de91db358fdf015511e4c3c8bc607c",
+    "url": "/SerhiiSkachkov/webby/static/media/Robotothin.72de91db.woff2"
+  },
+  {
+    "revision": "7faf23e11b3c2da59062fffc117e3a40",
+    "url": "/SerhiiSkachkov/webby/static/media/Robotothin.7faf23e1.ttf"
+  },
+  {
+    "revision": "a11d03f7bc9e68d84afdc5eea52ed121",
+    "url": "/SerhiiSkachkov/webby/static/media/Robotothin.a11d03f7.woff"
+  }
+]);
